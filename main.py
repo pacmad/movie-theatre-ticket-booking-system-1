330f64d9bc2edd38e075c4a2a13306e6fb1266f5
Rishabh <jainr214@gmail.com>
2020-08-29 15:18:02 +0530

    3 functions done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pymongo
+from bson.objectid import ObjectId
 
 
 client=pymongo.MongoClient('mongodb://127.0.0.1:27017/')
@@ -16,11 +17,22 @@ def book_ticket():
         'showtime':showtime,
         # "$currentDate":{'booked at':True}
         }
-
     coll.insert_one(test)
 
 
 
+def view_all(time):
+    for i in coll.find({'showtime':time}):
+        print(i)
 
-
+def time():
+    for i in coll.find():
+        print(i)
+        id=i['_id']
+        print(id)
+        print(id.generation_time)
+    
 book_ticket()
+view_all("19:30")
+
+time()
