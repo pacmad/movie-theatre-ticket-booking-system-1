26f4992981a0fd95a0f236671540c78385713b37
Rishabh <jainr214@gmail.com>
2020-08-29 07:11:07 +0530

    ticket booking

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-g
+import pymongo
+
+
+client=pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+mydb=client['tickets']
+coll=mydb.collection
+
+def book_ticket():
+    name=input('name ')
+    num=input('Number ')
+    showtime=input('time ')
+
+    test={
+        'name':name,
+        'number':num,
+        'showtime':showtime,
+        # "$currentDate":{'booked at':True}
+        }
+
+    coll.insert_one(test)
+
+
+
+
+
+book_ticket()
